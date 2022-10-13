d8d06fc5fae0b4702f92e6fd8e21e825fa829c26
Ogom-I <97358425+Ogom-I@users.noreply.github.com>
2022-10-13 19:49:08 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90,
+    crop_width:300,
+    crop_height:250
+
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach(camera);
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+      });
+} 
+function modelLoaded() 
+{
+    console.log("model is loaded")
+}
+console.log('ml5 version'+ ml5.version);
